831a24005ae3fd8e711d75f0fb8ce98b9e598b93
jelena2803 <jelenaiva2803@gmail.com>
2023-12-28 01:00:52 +0200

    Added properties of month names and birth days for sorting and filtering purposes

--- src/data.js
+++ src/data.js
@@ -1,12 +1,37 @@
 const data = [
-  { id: 1, name: "Tom Hags", birthday: "31 May" },
-  { id: 2, name: "Ahsgs HAgs", birthday: "10 February" },
-  { id: 3, name: "Ahsgs HAgs", birthday: "23 September" },
-  { id: 4, name: "Ahsgs HAgs", birthday: "15 July" },
-  { id: 5, name: "Ahsgs HAgs", birthday: "6 August" },
-  { id: 6, name: "Ahsgs HAgs", birthday: "17 October" },
-  { id: 7, name: "Ahsgs HAgs", birthday: "27 March" },
-  { id: 8, name: "Ahsgs HAgs", birthday: "13 April" },
+  { id: 1, name: "Tom Hags", birthday: "May 31", month: "May", day: 31 },
+  {
+    id: 2,
+    name: "Ahsgs HAgs",
+    birthday: "February 10",
+    month: "February",
+    day: 10,
+  },
+  {
+    id: 3,
+    name: "Ahsgs HAgs",
+    birthday: "September 23",
+    month: "September",
+    day: 23,
+  },
+  { id: 4, name: "Ahsgs HAgs", birthday: "July 15", month: "July", day: 15 },
+  { id: 5, name: "Ahsgs HAgs", birthday: "August 6", month: "August", day: 6 },
+  {
+    id: 6,
+    name: "Ahsgs HAgs",
+    birthday: "October 17",
+    month: "October",
+    day: 17,
+  },
+  { id: 7, name: "Ahsgs HAgs", birthday: "March 27", month: "March", day: 27 },
+  { id: 8, name: "Ahsgs HAgs", birthday: "April 13", month: "April", day: 13 },
+  {
+    id: 9,
+    name: "Ahsgs HAgs",
+    birthday: "January 13",
+    month: "January",
+    day: 13,
+  },
 ];
 
 export default data;
